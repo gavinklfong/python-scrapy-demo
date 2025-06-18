b50f46764deb471665e94e2f477fdae5b38b160f
Gavin Fong <gavinklfong@gmail.com>
2025-06-18 21:00:47 +0100

    init commit

---
+++ crawler/crawler/spiders/openlibrary.py
@@ -0,0 +1,34 @@
+import scrapy
+
+
+def extract_with_css(item, query):
+    return item.css(query).get(default="").strip()
+
+class OpenLibrarySpider(scrapy.Spider):
+    name = "openlibrary"
+
+    start_urls = ["https://openlibrary.org/trending/daily"]
+
+    def start_requests(self):        
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse_book_list)
+            
+
+    def parse_book_list(self, response):
+        book_items = response.css("#contentBody > div.mybooks-list > ul > li > div > div.details")
+        if not book_items:
+            self.logger.warning("No book items found on the page.")
+            return
+        
+        # Extract book details
+        for item in book_items:
+            yield {
+                "title": item.css('h3.booktitle > a::text').get(default='').strip(),
+                "author": extract_with_css(item, "span.bookauthor > a::text"),
+                "published_date": extract_with_css(item, "span.resultStats > span.resultDetails > span:nth-child(1)::text"),
+            }
+
+        # Handle pagination
+        next_page = response.css("#contentBody > div.pager > div > a:last-child::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, self.parse_book_list)
